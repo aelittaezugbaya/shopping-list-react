dc8f785f0075ffff8f1fac1c17c3e1caa66866ce
Aelitta Ezugbaya <aelittae@gmail.com>
2017-11-20 14:42:22 +0200

    rewrite input component and add reference to input for next work with it

--- frontend/components/Input.js
+++ frontend/components/Input.js
@@ -5,20 +5,27 @@ import React from 'react';
 import {ListGroupItem ,FormGroup,FormControl,Button} from 'react-bootstrap'
 
 export default class Input extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.onSubmit=this.onSubmit.bind(this);
+  }
+  onSubmit(ev){
+    ev.preventDefault();
+    console.log(this.input.value)
+  }
   render(){
     return(
       <ListGroupItem>
-        <form>
-          <FormGroup>
+        <form onSubmit={this.onSubmit}>
             <div className="row-fluid">
               <div className="col-md-11 col-sm-10 col-xs-8">
-                <FormControl type="text" placeholder="Enter item and press button add"/>
+                <input ref={ref => this.input = ref} type="text" className="form-control" placeholder="Enter food name and press add"/>
               </div>
               <div className="text-right">
                 <Button bsStyle='primary' type="submit">Submit</Button>
               </div>
             </div>
-          </FormGroup>
         </form>
       </ListGroupItem>
     )
